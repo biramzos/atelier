ab01eb0db11efe3f69b6f79a1dcb3f403f7954cd
biramzos <b.ramis.2002@gmail.com>
2022-08-25 16:26:48 +0600

    "commit"

---
+++ src/main/java/com/mastera/atelier/Controllers/ApiController.java
@@ -0,0 +1,59 @@
+package com.mastera.atelier.Controllers;
+
+import com.mastera.atelier.DTO.UserRequest;
+import com.mastera.atelier.Models.Order;
+import com.mastera.atelier.Models.User;
+import com.mastera.atelier.Services.OrderService;
+import com.mastera.atelier.Services.UserService;
+import com.mastera.atelier.Token.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private OrderService orderService;
+
+    Token generator = new Token();
+
+    @PostMapping(value = "/login",produces = "application/json")
+    public Map<String,String> getCurrentUser(@RequestBody UserRequest request){
+        User user = userService.getUserByUsername(request.getUsername());
+        if(user != null){
+            if(user.getPassword().equals(request.getPassword())){
+                return new HashMap<String,String>(){{
+                    put("fullname",user.getFirstname() + " " + user.getLastname());
+                    put("username",user.getUsername());
+                    put("phone",user.getPhone());
+                    put("image","https://ateliermastera.herokuapp.com/images/" + user.getUsername());
+                    put("password",user.getPassword());
+                    put("token",generator.tokenByUsername(user.getUsername()));
+                    put("message","success");
+                }};
+            }
+            return new HashMap<String, String>(){{
+                put("message","Password is incorrect");
+            }};
+        }
+        return new HashMap<String, String>(){{
+            put("message","User does not found");
+        }};
+    }
+
+    @GetMapping(value = "/orders",produces = "application/json")
+    public List<Order> getOrders(){
+        return orderService.getAll();
+    }
+
+    @GetMapping(value = "/orders/{username}",produces = "application/json")
+    public List<Order> getOrdersByUsername(@PathVariable("username") String username){
+        return orderService.getOrdersByUsername(username);
+    }
+}
---
+++ src/main/java/com/mastera/atelier/DTO/UserRequest.java
@@ -0,0 +1,11 @@
+package com.mastera.atelier.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequest {
+    private String username;
+    private String password;
+}
